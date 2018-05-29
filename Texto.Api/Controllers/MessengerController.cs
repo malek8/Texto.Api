e01e15abeda9d0bd9770873fe9fda705c9452680
Malek Atwiz <matwiz@futurecode.ca>
2018-05-28 23:01:22 -0400

    Messenger controller

---
+++ Texto.Api/Controllers/MessengerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Texto.Api.Models;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Texto.Api.Controllers
+{
+    //[Route("api/[controller]")]
+    public class MessengerController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public MessengerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("api/v1/[controller]/send")]
+        [HttpPost]
+        public void Send([FromBody]MessageModel model)
+        {
+            var sid = _configuration["TwilioSmsCredentials:Sid"];
+            var token = _configuration["TwilioSmsCredentials:Token"];
+            var fromNumber = _configuration["TwilioSettings:FromNumber"];
+
+            TwilioClient.Init(sid, token);
+
+            var result = MessageResource.Create(from: new Twilio.Types.PhoneNumber(fromNumber),
+                to: new Twilio.Types.PhoneNumber(model.Number),
+                body: model.Message);
+        }
+    }
+}
---
+++ Texto.Api/Models/MessageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Texto.Api.Models
+{
+    public class MessageModel
+    {
+        public string Number { get; set; }
+        public string Message { get; set; }
+    }
+}
