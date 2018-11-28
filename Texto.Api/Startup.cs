1fe210f5f8b8aa0ea011509e84b587d48c26be86
Malek Atwiz <malekatwiz@gmail.com>
2018-11-27 21:27:11 -0500

    configuring logger

--- Texto.Api/Startup.cs
+++ Texto.Api/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Texto.Api.Services;
 
 namespace Texto.Api
@@ -34,8 +35,9 @@ namespace Texto.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("Logs/log-{Date}.txt");
             app.UseAuthentication();
 
             if (env.IsDevelopment())
